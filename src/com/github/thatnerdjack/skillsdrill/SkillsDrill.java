592bb3d9d2a3fb52b8ad65d4be91f760634a5f54
thatnerdjack <jackjyro@gmail.com>
2014-11-20 14:35:59 -0500

    wip

--- src/com/github/thatnerdjack/skillsdrill/SkillsDrill.java
+++ src/com/github/thatnerdjack/skillsdrill/SkillsDrill.java
@@ -5,6 +5,9 @@ package com.github.thatnerdjack.skillsdrill;
  */
 public class SkillsDrill {
 
+    public static final String VOWELS = "aeiou";
+    public static final String CONSONANTS = "bcdfghjklmnpqrstvwxyz";
+
     public static void factorial(int input) {
         int total = 1;
         for(int i = 1; i <= input; i++) {
@@ -23,9 +26,47 @@ public class SkillsDrill {
         System.out.println(returnString);
     }
 
+    public static int randomInt(int max) {
+        return (int)(Math.random()*max + 1);
+    }
+
+    public static int[] arrayFill(int size, int max) {
+        int[] returnArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            returnArray[i] = randomInt(max);
+        }
+        return returnArray;
+    }
+
+    public static void printArray(int[] a) {
+        String returnString = "";
+        for (int i = 0; i < a.length; i++) {
+            returnString += a[i] + " ";
+        }
+        System.out.println(returnString);
+    }
+
+    public static void findMaxInArray(int[] a) {
+        int currentMax = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (currentMax < a[i]) {
+                currentMax = a[i];
+            }
+        }
+        System.out.println(currentMax);
+    }
+
+    public static void randomWord() {
+        String returnString = "";
+
+    }
+
     public static void main(String args[]) {
         factorial(4);
         stripNonLetters("You can't always get what you want, buddy!!");
+        int[] filledArray = arrayFill(10, 50);
+        printArray(filledArray);
+        findMaxInArray(filledArray);
     }
 
 }
